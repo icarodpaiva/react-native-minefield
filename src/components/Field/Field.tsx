3a0112ebcad7a2319e801357a5faf66fc6967b9d
Icaro Paiva <icaro.paiva@acct.global>
2023-05-15 21:59:42 -0300

    field and board in progress

---
+++ src/components/Field/Field.tsx
@@ -0,0 +1,82 @@
+import React from "react"
+import { View, Text, StyleSheet } from "react-native"
+import type { StyleProp, ViewStyle } from "react-native"
+
+import { params } from "../../params"
+
+interface FieldProps {
+  opened?: boolean
+  mined?: boolean
+  nearMines?: number
+}
+
+export const Field = ({
+  mined = false,
+  opened = false,
+  nearMines = 0
+}: FieldProps) => {
+  const styleField: StyleProp<ViewStyle> = [styles.field]
+
+  if (opened) {
+    styleField.push(styles.opened)
+  }
+
+  if (styleField.length === 1) {
+    styleField.push(styles.regular)
+  }
+
+  const getColor = () => {
+    if (nearMines <= 0) {
+      return ""
+    }
+
+    if (nearMines === 1) {
+      return "#2A28D7"
+    }
+
+    if (nearMines === 2) {
+      return "#2B520F"
+    }
+
+    if (nearMines > 2 && nearMines < 6) {
+      return "#F9060A"
+    }
+
+    if (nearMines >= 6) {
+      return "#F221A9"
+    }
+  }
+
+  return (
+    <View style={styleField}>
+      {!mined && opened && nearMines > 0 && (
+        <Text style={[styles.label, { color: getColor() }]}>{nearMines}</Text>
+      )}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  field: {
+    width: params.blockSize,
+    height: params.blockSize,
+    borderWidth: params.borderSize
+  },
+  regular: {
+    backgroundColor: "#999",
+    borderTopColor: "#cccc",
+    borderRightColor: "#333",
+    borderBottomColor: "#333",
+    borderLeftColor: "#cccc"
+  },
+  opened: {
+    backgroundColor: "#999",
+    borderColor: "#777",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  label: {
+    fontWeight: "bold",
+    fontSize: params.fontSize
+  }
+})
